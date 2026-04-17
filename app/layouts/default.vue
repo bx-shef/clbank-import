b8ac6a5585f5048ff419e690b9e2af0f5bf440c7
Igor Shevchik <igor@bx-shef.by>
2026-04-17 08:32:29 +0300

    chore(clear): remove cookie

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,35 +1,9 @@
 <script setup lang="ts">
 import type { Ref } from 'vue'
-import { ref, inject, onMounted } from 'vue'
+import { ref, inject } from 'vue'
 
-const toast = useToast()
 const isLoading = inject<Ref<boolean>>('isLoading', ref(false))
 
-onMounted(async () => {
-  const cookie = useCookie('cookie-consent')
-  if (cookie.value === 'accepted') {
-    return
-  }
-
-  toast.add({
-    title: 'We use first-party cookies to enhance your experience on our app.',
-    duration: 0,
-    close: false,
-    actions: [
-      {
-        label: 'Accept',
-        color: 'air-primary-success',
-        onClick: () => {
-          cookie.value = 'accepted'
-        }
-      },
-      {
-        label: 'Opt out',
-        color: 'air-secondary-no-accent'
-      }
-    ]
-  })
-})
 </script>
 
 <template>
